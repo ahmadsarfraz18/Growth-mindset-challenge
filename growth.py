f1fd64943a7282ff716cd577a5e5924f4d35d39d
ahmadsarfraz18 <160363481+ahmadsarfraz18@users.noreply.github.com>
2025-02-24 05:15:56 +0500

    growth.py

--- growth.py
+++ growth.py
@@ -13,8 +13,8 @@ st.markdown(
     """
     <style>
     .stApp{
-        background-color: blue;
-        color: yellow
+        background-color: black;
+        color: white
         }
         </style>
 
